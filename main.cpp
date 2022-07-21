31a231f9ecec73ed2abf965af9892466eddf94b2
Josue Ventura <josueventura2002@gmail.com>
2022-07-20 23:57:08 -0600

    Tarea Raiz Cuadrada

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+  
+ {
+ 	//Declaracion de variables
+    float numero,raiz;
+    
+    //Ingreso de dato
+    cout<<"ingrese un numero:";
+	cin >>numero;
+	
+	//Proceso y salida.
+    raiz=sqrt(numero);
+    cout << " la raiz cuadra de " << numero << " es: " << raiz;
+    
+    
+  return 0;
+ }
+
+	
+	
+	
+	
+	
+	return 0;
+}
